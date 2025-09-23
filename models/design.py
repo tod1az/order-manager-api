6f3430b3d6018bac92e556222e99413f3cf82ff5
tod1az <t.diaz.soto@gmail.com>
2025-09-23 19:09:25 -0300

    fix: make image atribute of Design model an array of string

--- models/design.py
+++ models/design.py
@@ -1,16 +1,18 @@
+from sqlalchemy import ARRAY
 from models.db import db
 
 class Design(db.Model):
      id = db.Column(db.Integer, primary_key= True, autoincrement=True)
      name = db.Column(db.String(255), nullable=False)
-     image= db.Column(db.String(255), nullable=False)
+     images= db.Column(ARRAY(db.String),nullable=True, default=[])
      price = db.Column(db.Integer,nullable=False)
 
      def to_dict(self):
         return {
             "id":self.id,
             "description":self.name,
-            "price":self.price
+            "price":self.price,
+            "images":self.images
         }
 
  
