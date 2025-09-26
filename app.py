14fd6fc9afb73a79e2635b916c771655b97dfed6
tod1az <t.diaz.soto@gmail.com>
2025-09-26 13:51:13 -0300

    feat:add cors config

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
-from models.db import db 
+from models.db import db
 from routes.user import users_bp
 from routes.design import designs_bp
 from routes.garment import garments_bp
-from routes.garment_variant import garments_variant_bp 
-from routes.order import orders_bp 
+from routes.garment_variant import garments_variant_bp
+from routes.order import orders_bp
 from routes.item import items_bp
 from routes.auth import auth_bp
 from extensions import bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 load_dotenv()
 
@@ -21,18 +22,33 @@ app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
-app.config["JWT_COOkIE_SECURE"] = False
+app.config["JWT_COOKIE_SECURE"] = False
 app.config["JWT_HTTPONLY"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": ["http://localhost:5173", "https://mi-frontend.com"],
+            "methods": ["GET", "POST", "PUT", "DELETE"],
+            "allow_headers": ["Content-Type", "Authorization"],
+            "support_credentials": True,
+        }
+    },
+)
+
 
 jwt = JWTManager(app)
 db.init_app(app)
 bcrypt.init_app(app)
 
+
 @app.route("/")
 def home():
     return "Bienvenido a la API de gestor de ordenes"
+
 
 app.register_blueprint(users_bp)
 app.register_blueprint(designs_bp)
@@ -43,10 +59,9 @@ app.register_blueprint(items_bp)
 app.register_blueprint(auth_bp)
 
 
-
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
-    #app.run(debug=True, host="0.0.0.0", port=8082)
+    # app.run(debug=True, host="0.0.0.0", port=8082)
