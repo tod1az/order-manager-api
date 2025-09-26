fd06d38a0bbfcabef3199b372b1974a4967ea4ef
tod1az <t.diaz.soto@gmail.com>
2025-09-26 15:15:01 -0300

    feat:add refresh token

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, request, jsonify
 from models.user import User
 from extensions import bcrypt
-from flask_jwt_extended import create_access_token, jwt_required, set_access_cookies
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    set_access_cookies,
+    set_refresh_cookies,
+)
 from datetime import timedelta
 
 from routes.utils.auth import auth_info
@@ -27,20 +33,57 @@ def login():
             return jsonify({"error": "Email and password are required"}), 400
 
         user = User.query.filter_by(email=email).first()
-        additional_claims = {"role": user.role, "email": user.email, "name": user.name}
+        print(user)
+        additional_claims = {
+            "role": user.role,
+            "email": user.email,
+            "name": user.name,
+        }
         if user and bcrypt.check_password_hash(user.password, password):
             access_token = create_access_token(
                 identity=str(user.id),
                 additional_claims=additional_claims,
                 expires_delta=timedelta(hours=1),
             )
+            refresh_token = create_refresh_token(
+                identity=str(user.id),
+                additional_claims=additional_claims,
+                expires_delta=timedelta(days=7),
+            )
             response = jsonify({"message": "Login successful"})
             set_access_cookies(response, access_token)
+            set_refresh_cookies(response, refresh_token)
             return response, 200
         else:
             return jsonify({"error": "Invalid credentials"}), 401
     except Exception as e:
         return jsonify({"error": f"Something went wrong: {str(e)}"}), 500
+
+
+@auth_bp.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    user = auth_info()
+    additional_claims = {
+        "role": user.get("role"),
+        "email": user.get("email"),
+        "name": user.get("name"),
+    }
+    new_access_token = create_access_token(
+        identity=str(user.get("id")),
+        additional_claims=additional_claims,
+        expires_delta=timedelta(hours=1),
+    )
+    refresh_token = create_refresh_token(
+        identity=str(user.get("id")),
+        additional_claims=additional_claims,
+        expires_delta=timedelta(days=7),
+    )
+
+    response = jsonify({"message": "Token refreshed"})
+    set_access_cookies(response, new_access_token)
+    set_refresh_cookies(response, refresh_token)
+    return response, 200
 
 
 @auth_bp.route("/logout", methods=["POST"])
