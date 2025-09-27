b66b55149a5fdaaea3b425b49c3f9e9c0f2d4ebd
tod1az <t.diaz.soto@gmail.com>
2025-09-27 13:10:58 -0300

    fix: make the email attribute unique for the user model

--- models/user.py
+++ models/user.py
@@ -1,28 +1,29 @@
 from models.db import db
 
+
 class User(db.Model):
-     id = db.Column(db.Integer, primary_key= True, autoincrement=True)
-     email = db.Column(db.String(255), nullable=False)
-     name = db.Column(db.String(255), nullable=False)
-     lastname = db.Column(db.String(255), nullable=False)
-     role = db.Column(db.String(255), nullable=False)
-     password  =db.Column(db.String(255),nullable=False)
-     
-     def  private_to_dict(self):
-        return{
-            "id":self.id,
-            "email":self.email,
-            "name":self.name,
-            "lastname":self.lastname,
-            "role":self.role,
-            "password":self.password,
-        }
-     def to_dict(self):
-        return{
-            "id":self.id,
-            "email":self.email,
-            "name":self.name,
-            "lastname":self.lastname,
-            "role":self.role,
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    name = db.Column(db.String(255), nullable=False)
+    lastname = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def private_to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            "role": self.role,
+            "password": self.password,
         }
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            "role": self.role,
+        }
